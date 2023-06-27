a89ea6d377e9b1cd4aa339f16f7d19f9dee92bf3
Leonardo <leonardodf95@gmail.com>
2023-06-26 22:08:06 -0300

    start of websocket to make synchronous server side with client side

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
+import createGame from "./public/game.js";
 
 const app = express();
 const server = http.createServer(app);
+const sockets = new Server(server);
 
 app.use(express.static("public"));
 
-server.listen(3000, () => {
-  console.log("Server listening on port: 3000");
+const game = createGame();
+
+console.log("state game", game.state);
+
+sockets.on("connection", (socket) => {
+  const playerId = socket.id;
+  console.log(`> Player connected on Server with id: ${playerId}`);
+
+  socket.emit("setup", game.state);
+});
+
+server.listen(3030, () => {
+  console.log("Server listening on port: 3030");
 });
